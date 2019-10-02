2bbe7cd12e815dd093ccfe1e817862e454f8405c
pcysl5edgo <pcysl5edgo@gmail.com>
2019-10-02 16:06:27 +0900

    exit

--- Assets/Editor/ExecuteMethods.cs
+++ Assets/Editor/ExecuteMethods.cs
@@ -8,6 +8,8 @@ public static class ExecuteMethods
     public static void CreateDll()
     {
         var settingWindow = EditorWindow.GetWindow<SettingWindow>();
+        if(settingWindow is null)
+            EditorApplication.Exit(12);
         settingWindow.Initialize();
         settingWindow.Execute();
     }
