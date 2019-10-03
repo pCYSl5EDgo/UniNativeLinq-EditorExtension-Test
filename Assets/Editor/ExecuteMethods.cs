09135dbd32e4327c0ab3d96a78c1cf0f802f691f
pcysl5edgo <pcysl5edgo@gmail.com>
2019-10-03 11:00:44 +0900

    single

--- Assets/Editor/ExecuteMethods.cs
+++ Assets/Editor/ExecuteMethods.cs
@@ -2,41 +2,14 @@
 using System.IO;
 using UniNativeLinq.Editor;
 using UnityEditor;
+using UnityEngine;
 
 public static class ExecuteMethods
 {
     public static void CreateDll()
     {
-        Console.WriteLine("THIS IS A TEST");
-        SettingWindow settingWindow = default;
-        try
-        {
-            Console.WriteLine("DECL");
-            settingWindow = new SettingWindow();
-            Console.WriteLine("DECL 2");
-        }
-        catch (System.Exception e)
-        {
-            Console.WriteLine("CATCH 2");
-            Console.WriteLine(e.ToString());
-            throw;
-        }
-        Console.WriteLine("THIS IS A TEST 2");
-        if(settingWindow is null){
-            Console.WriteLine("NULL!!!");
-            EditorApplication.Exit(12);
-        }
-        try
-        {
-            Console.WriteLine("THIS IS A TEST 3");
-            settingWindow.Initialize();
-            Console.WriteLine("THIS IS A TEST 4");
-            settingWindow.Execute();
-        }
-        catch(Exception e)
-        {
-            Console.WriteLine(e.ToString());
-            throw;
-        }
+        var settingWindow = Application.isBatchMode ? new SettingWindow() : EditorWindow.GetWindow<SettingWindow>();
+        settingWindow.Initialize();
+        settingWindow.Execute();
     }
 }
