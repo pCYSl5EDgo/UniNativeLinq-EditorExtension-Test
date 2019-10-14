86d26bc487d141777bd62b43a0357e3478ae0542
pcysl5edgo <pcysl5edgo@gmail.com>
2019-10-14 20:29:17 +0900

    [update]Create Unity Package

--- Assets/Editor/ExecuteMethods.cs
+++ Assets/Editor/ExecuteMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.IO;
 using UniNativeLinq.Editor;
 using UnityEditor;
@@ -11,5 +12,27 @@ public static class ExecuteMethods
         var settingWindow = Application.isBatchMode ? new SettingWindow() : EditorWindow.GetWindow<SettingWindow>();
         settingWindow.Initialize();
         settingWindow.Execute();
+    }
+
+    public static void CreateUnityPackage()
+    {
+        // configure
+        var root = "Scripts/UNL";
+        var exportPath = "../artifact/UniNativeLinq.unitypackage";
+ 
+        var path = Path.Combine(Application.dataPath, root);
+        var assets = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
+            .Where(x => Path.GetExtension(x) == ".cs")
+            .Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"))
+            .ToArray();
+ 
+        UnityEngine.Debug.Log("Export below files" + Environment.NewLine + string.Join(Environment.NewLine, assets));
+ 
+        AssetDatabase.ExportPackage(
+            assets,
+            exportPath,
+            ExportPackageOptions.Default);
+ 
+        UnityEngine.Debug.Log("Export complete: " + Path.GetFullPath(exportPath));
     }
 }
